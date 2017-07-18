b29678e621d3a965529737a43c3895f65a99ba20
R.I.Pienaar <rip@devco.net>
2017-07-18 12:18:08 +0200

    (#12) Federation Broker
    This adds:
    
      * A working federation broker thats compatible with the ruby one
      * A basic connector, missing a bunch of things to be like mcollective
        but enough for this need
      * Various tweaks and cleanups to the schemas and such

--- logger.go
+++ logger.go
@@ -5,33 +5,37 @@ import (
 )
 
 // Logger is nats server.Logger compatible logging wrapper for logrus
-type Logger struct{}
+type Logger struct {
+	log *log.Entry
+}
 
 // Noticef logs at info level
 func (l Logger) Noticef(format string, v ...interface{}) {
-	log.Infof(format, v...)
+	l.log.Infof(format, v...)
 }
 
 // Fatalf logs at fatal level
 func (l Logger) Fatalf(format string, v ...interface{}) {
-	log.Fatalf(format, v...)
+	l.log.Fatalf(format, v...)
 }
 
 // Errorf logs at error lovel
 func (l Logger) Errorf(format string, v ...interface{}) {
-	log.Errorf(format, v...)
+	l.log.Errorf(format, v...)
 }
 
 // Debugf logs at debug level
 func (l Logger) Debugf(format string, v ...interface{}) {
-	log.Debugf(format, v...)
+	l.log.Debugf(format, v...)
 }
 
 // Tracef logs at debug level
 func (l Logger) Tracef(format string, v ...interface{}) {
-	log.Debugf(format, v...)
+	l.log.Debugf(format, v...)
 }
 
 func newLogger() Logger {
-	return Logger{}
+	return Logger{
+		log: log.WithFields(log.Fields{"component": "broker"}),
+	}
 }
