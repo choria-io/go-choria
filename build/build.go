60228a9bbbfc0f1176df6156a57a73b388134c4f
R.I.Pienaar <rip@devco.net>
2017-12-14 12:04:40 +0100

    (#90) Emit build info on /choria

--- build/build.go
+++ build/build.go
@@ -12,8 +12,18 @@ var BuildDate = "unknown"
 // License is the official Open Source Initiave license abbreciation
 var License = "Apache-2.0"
 
-// Set to default to no TLS
+// TLS controls the NATS protocol level TLS
 var TLS = "true"
 
-// Set to disable signing and certificates in the protocol
+// Secure controls the signing and validations of certificates in the protocol
 var Secure = "true"
+
+// IsSecure determines if this build will validate senders at protocol level
+func IsSecure() bool {
+	return Secure == "true"
+}
+
+// HasTLS determines if TLS should be used on the wire
+func HasTLS() bool {
+	return TLS == "true"
+}
