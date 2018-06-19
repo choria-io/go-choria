10cbe0dc48307e01aac535ab2b22952aa0571a56
R.I.Pienaar <rip@devco.net>
2018-06-19 14:51:18 +0200

    (#350) initial mco rpc client
    This adds an initial mcollective compatible RPC client to the mcorpc
    package
    
    It supports batched and unbatched modes, discovery, keeps stats a lot
    like the old one does and mimic it in many regards though there are
    inevitable differences
    
    DDL handling is missing

---
+++ client/util.go
@@ -0,0 +1,58 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math"
+	"time"
+)
+
+// ParseReplyData parses reply data and populates a Reply and custom Data
+func ParseReplyData(source []byte) (*RPCReply, error) {
+	reply := &RPCReply{}
+
+	err := json.Unmarshal(source, reply)
+	if err != nil {
+		return reply, fmt.Errorf("could not decode source data: %s", err)
+	}
+
+	return reply, nil
+}
+
+// InGroups calls f for sub slices of a slice where every slice
+// is at most `size` big
+func InGroups(set []string, size int, f func([]string) error) error {
+	count := math.Ceil(float64(len(set)) / float64(size))
+
+	for i := 0; i < int(count); i++ {
+		start := i * int(size)
+		end := start + int(size)
+
+		if end > len(set) {
+			end = len(set)
+		}
+
+		err := f(set[start:end])
+		if err != nil {
+			return fmt.Errorf("failed to access subset group %d:%d %s", start, end, err)
+		}
+	}
+
+	return nil
+}
+
+// InterruptableSleep sleep for the duration of the n'th wait cycle
+// in a way that can be interrupted by the context.  An error is returned
+// if the context cancels the sleep
+func InterruptableSleep(ctx context.Context, d time.Duration) error {
+	timer := time.NewTimer(d)
+
+	select {
+	case <-timer.C:
+		return nil
+	case <-ctx.Done():
+		return errors.New("sleep interrupted by context")
+	}
+}
