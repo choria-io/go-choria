be97f283acff2f1f728f3baa0736a3193fb6c090
R.I.Pienaar <rip@devco.net>
2018-08-03 15:01:27 +0200

    (#398) add ProvisionReprovisionToken

--- choria/framework.go
+++ choria/framework.go
@@ -74,10 +74,8 @@ func (fw *Framework) setupSecurity() error {
 
 	switch fw.Config.Choria.SecurityProvider {
 	case "puppet":
-		fw.log.Info("Configuring Choria using the puppet security subsystem")
 		fw.security, err = puppetsec.New(puppetsec.WithResolver(fw), puppetsec.WithChoriaConfig(fw.Config), puppetsec.WithLog(fw.Logger("security")))
 	case "file":
-		fw.log.Info("Configuring Choria using the file security subsystem")
 		fw.security, err = filesec.New(filesec.WithChoriaConfig(fw.Config), filesec.WithLog(fw.Logger("security")))
 	default:
 		err = fmt.Errorf("unknown security provider %s", fw.Config.Choria.SecurityProvider)
