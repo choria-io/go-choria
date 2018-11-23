c6c70f20a868631ca6ca2c686f91dcb87c057c93
R.I.Pienaar <rip@devco.net>
2018-11-23 22:17:08 +0100

    (#22) support slashes in regex
    In the ruby code we support regex config like 'foo, /bar/' this was
    not supported here, this change supports those in the same way the
    ruby side did

--- filesec/util.go
+++ filesec/util.go
@@ -4,11 +4,17 @@ import (
 	"os"
 	"regexp"
 	"runtime"
+	"strings"
 )
 
 // MatchAnyRegex checks str against a list of possible regex, if any match true is returned
 func MatchAnyRegex(str []byte, regex []string) bool {
 	for _, reg := range regex {
+		if matched, _ := regexp.MatchString("^/.+/$", reg); matched {
+			reg = strings.TrimLeft(reg, "/")
+			reg = strings.TrimRight(reg, "/")
+		}
+
 		if matched, _ := regexp.Match(reg, str); matched {
 			return true
 		}
