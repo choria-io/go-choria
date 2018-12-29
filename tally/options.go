11769c23da7a1e01a7c9f628659e5caa16a45de4
R.I.Pienaar <rip@devco.net>
2018-12-29 18:07:11 +0100

    (#20) add a tally package

---
+++ tally/options.go
@@ -0,0 +1,78 @@
+package tally
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+// options are options that configure the tool
+type options struct {
+	Component  string
+	Debug      bool
+	Log        logrus.StdLogger
+	Connector  Connector
+	StatPrefix string
+}
+
+// Option configures Options
+type Option func(*options)
+
+// Validate validates options meet minimal requirements, also assigns defaults
+// for optional settings
+func (o *options) Validate() error {
+	if o.Component == "" {
+		return fmt.Errorf("needs a component")
+	}
+
+	if o.Connector == nil {
+		return fmt.Errorf("needs a connector")
+	}
+
+	if o.StatPrefix == "" {
+		o.StatPrefix = fmt.Sprintf("lifecycle_tally")
+	}
+
+	if o.Log == nil {
+		o.Log = log.New(os.Stdout, "tally", 0)
+	}
+
+	return nil
+}
+
+// Component sets the component to tally
+func Component(c string) Option {
+	return func(o *options) {
+		o.Component = c
+	}
+}
+
+// Debug enable debug logging
+func Debug() Option {
+	return func(o *options) {
+		o.Debug = true
+	}
+}
+
+// Logger is the logger to use
+func Logger(l logrus.StdLogger) Option {
+	return func(o *options) {
+		o.Log = l
+	}
+}
+
+// Connection is the middleware to receive events on
+func Connection(c Connector) Option {
+	return func(o *options) {
+		o.Connector = c
+	}
+}
+
+// StatsPrefix is the space to create stat entries in
+func StatsPrefix(p string) Option {
+	return func(o *options) {
+		o.StatPrefix = p
+	}
+}
---
+++ tally/tally_test.go
@@ -0,0 +1,133 @@
+package tally
+
+import (
+	"io/ioutil"
+	"time"
+
+	"github.com/choria-io/go-lifecycle"
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+
+	"testing"
+)
+
+func TestChoria(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Tally")
+}
+
+var _ = Describe("Tally", func() {
+	var (
+		logger   = logrus.NewEntry(logrus.New())
+		recorder *Recorder
+	)
+
+	BeforeEach(func() {
+		logger.Logger.SetOutput(ioutil.Discard)
+		registerStats = false
+		recorder = &Recorder{
+			observed: make(map[uint64]*observation),
+			options: &options{
+				Component:  "ginkgo",
+				StatPrefix: "tally",
+				Log:        logger,
+			},
+		}
+		recorder.createStats()
+	})
+
+	Describe("maintenance", func() {
+		It("Should not delete current nodes", func() {
+			event, err := lifecycle.New(lifecycle.Alive, lifecycle.Component("ginkgo"), lifecycle.Version("1.2.3"), lifecycle.Identity("ginkgo.example.net"))
+			Expect(err).ToNot(HaveOccurred())
+
+			recorder.process(event)
+			Expect(recorder.observed).To(HaveLen(1))
+			recorder.maintenance()
+			Expect(recorder.observed).To(HaveLen(1))
+		})
+
+		It("Should not delete old nodes", func() {
+			event, err := lifecycle.New(lifecycle.Alive, lifecycle.Component("ginkgo"), lifecycle.Version("1.2.3"), lifecycle.Identity("ginkgo.example.net"))
+			Expect(err).ToNot(HaveOccurred())
+
+			recorder.process(event)
+			Expect(recorder.observed).To(HaveLen(1))
+
+			h := hostHash("ginkgo.example.net")
+			recorder.observed[h].ts = recorder.observed[h].ts.Add(-1 * (time.Hour + 2*time.Minute))
+			recorder.maintenance()
+			Expect(recorder.observed).To(HaveLen(0))
+		})
+	})
+
+	Describe("process", func() {
+		It("Should handle new hosts", func() {
+			event, err := lifecycle.New(lifecycle.Alive, lifecycle.Component("ginkgo"), lifecycle.Version("1.2.3"), lifecycle.Identity("ginkgo.example.net"))
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(recorder.observed).To(HaveLen(0))
+			recorder.process(event)
+			Expect(recorder.observed).To(HaveLen(1))
+
+			h := hostHash("ginkgo.example.net")
+			Expect(recorder.observed[h].version).To(Equal("1.2.3"))
+			Expect(getPromValue(recorder.eventsTally, "ginkgo", "1.2.3")).To(Equal(1.0))
+		})
+
+		It("Should handle old hosts", func() {
+			event, err := lifecycle.New(lifecycle.Alive, lifecycle.Component("ginkgo"), lifecycle.Version("1.2.3"), lifecycle.Identity("ginkgo.example.net"))
+			Expect(err).ToNot(HaveOccurred())
+
+			recorder.process(event)
+			Expect(recorder.observed).To(HaveLen(1))
+
+			h := hostHash("ginkgo.example.net")
+			Expect(recorder.observed[h].version).To(Equal("1.2.3"))
+			Expect(getPromValue(recorder.eventsTally, "ginkgo", "1.2.3")).To(Equal(1.0))
+
+			recorder.process(event)
+
+			Expect(recorder.observed).To(HaveLen(1))
+			Expect(recorder.observed[h].version).To(Equal("1.2.3"))
+			Expect(getPromValue(recorder.eventsTally, "ginkgo", "1.2.3")).To(Equal(1.0))
+		})
+
+		It("Should handle updated hosts", func() {
+			event, err := lifecycle.New(lifecycle.Alive, lifecycle.Component("ginkgo"), lifecycle.Version("1.2.3"), lifecycle.Identity("ginkgo.example.net"))
+			Expect(err).ToNot(HaveOccurred())
+
+			h := hostHash("ginkgo.example.net")
+
+			recorder.process(event)
+			Expect(recorder.observed[h].version).To(Equal("1.2.3"))
+			Expect(getPromValue(recorder.eventsTally, "ginkgo", "1.2.3")).To(Equal(1.0))
+
+			event, err = lifecycle.New(lifecycle.Alive, lifecycle.Component("ginkgo"), lifecycle.Version("1.2.4"), lifecycle.Identity("ginkgo.example.net"))
+			Expect(err).ToNot(HaveOccurred())
+
+			recorder.process(event)
+			Expect(recorder.observed[h].version).To(Equal("1.2.4"))
+			Expect(getPromValue(recorder.eventsTally, "ginkgo", "1.2.2")).To(Equal(0.0))
+			Expect(getPromValue(recorder.eventsTally, "ginkgo", "1.2.4")).To(Equal(1.0))
+		})
+	})
+})
+
+func getPromValue(ctr *prometheus.GaugeVec, labels ...string) float64 {
+	pb := &dto.Metric{}
+	m, err := ctr.GetMetricWithLabelValues(labels...)
+	if err != nil {
+		return 0
+	}
+
+	if m.Write(pb) != nil {
+		return 0
+	}
+
+	return pb.GetGauge().GetValue()
+}
