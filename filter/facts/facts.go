ff6f6b3dcf378f2fcae5c789aee5d425aa6d426e
R.I.Pienaar <rip@devco.net>
2019-12-23 23:00:52 +0100

    (#38) add more fact match helpers

--- filter/facts/facts.go
+++ filter/facts/facts.go
@@ -19,13 +19,13 @@ type Logger interface {
 	Errorf(format string, args ...interface{})
 }
 
-// MatchFile match fact filters in a OR manner, only nodes that have all the matching facts will be true here
-func MatchFile(filters [][3]string, file string, log Logger) bool {
+// MatchFacts match fact filters in a OR manner, only facts matching all filters will be true
+func MatchFacts(filters [][3]string, facts json.RawMessage, log Logger) bool {
 	matched := false
 	var err error
 
 	for _, filter := range filters {
-		matched, err = HasFact(filter[0], filter[1], filter[2], file, log)
+		matched, err = HasFactJSON(filter[0], filter[1], filter[2], facts, log)
 		if err != nil {
 			log.Warnf("Failed to match fact '%#v': %s", filter, err)
 			return false
@@ -38,6 +38,17 @@ func MatchFile(filters [][3]string, file string, log Logger) bool {
 	}
 
 	return matched
+}
+
+// MatchFile match fact filters in a OR manner, only nodes that have all the matching facts will be true here
+func MatchFile(filters [][3]string, file string, log Logger) bool {
+	facts, err := JSON(file, log)
+	if err != nil{
+		log.Warnf("Failed to match fact: '%#v': %s", filters, err)
+		return false
+	}
+
+	return MatchFacts(filters, facts, log)
 }
 
 // JSON parses the data, including doing any conversions needed, and returns JSON text
@@ -103,41 +114,54 @@ func GetFact(fact string, file string, log Logger) ([]byte, gjson.Result, error)
 		return nil, gjson.Result{}, err
 	}
 
-	found := gjson.GetBytes(j, fact)
-	if !found.Exists() {
-		return nil, gjson.Result{}, nil
-	}
-
+	found, err := GetFactJSON(fact, j)
 	return j, found, nil
 }
 
-// HasFact evaluates the expression against facts in the file
-func HasFact(fact string, operator string, value string, file string, log Logger) (bool, error) {
-	j, found, err := GetFact(fact, file, log)
-	if err != nil {
+// GetFactJSON looks up a single fact from the JSON data, absent fact is handled as empty
+// result and no error
+func GetFactJSON(fact string, facts json.RawMessage) (gjson.Result, error) {
+	result := gjson.GetBytes(facts, fact)
+
+	return result, nil
+}
+
+func HasFactJSON(fact string, operator string, value string, facts json.RawMessage, log Logger) (bool, error){
+	result, err := GetFactJSON(fact, facts)
+	if err != nil{
 		return false, err
 	}
 
-	if !found.Exists() {
+	if !result.Exists() {
 		return false, nil
 	}
 
 	switch operator {
 	case "==":
-		return eqMatch(found, value, &j)
+		return eqMatch(result, value)
 	case "=~":
-		return reMatch(found, value, &j)
+		return reMatch(result, value)
 	case "<=":
-		return leMatch(found, value, &j)
+		return leMatch(result, value)
 	case ">=":
-		return geMatch(found, value, &j)
+		return geMatch(result, value)
 	case "<":
-		return ltMatch(found, value, &j)
+		return ltMatch(result, value)
 	case ">":
-		return gtMatch(found, value, &j)
+		return gtMatch(result, value)
 	case "!=":
-		return neMatch(found, value, &j)
+		return neMatch(result, value)
 	default:
 		return false, fmt.Errorf("unknown fact matching operator %s while looking for fact %s", operator, fact)
 	}
+}
+
+// HasFact evaluates the expression against facts in the file
+func HasFact(fact string, operator string, value string, file string, log Logger) (bool, error) {
+	j, err := JSON(file, log)
+	if err != nil {
+		return false, err
+	}
+
+	return HasFactJSON(fact, operator, value, j, log)
 }
