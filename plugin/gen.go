56409a07467acdd27ccd7e76f020a1e1a8283922
R.I.Pienaar <rip@devco.net>
2019-12-31 10:33:20 +0100

    (misc) go fmt generated files and update dependencies

--- plugin/gen.go
+++ plugin/gen.go
@@ -6,13 +6,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/choria-io/go-choria/plugin"
 )
 
 var ctr = 1
 
-func generateLoader(p *plugin.Plugin) error {
+func generateLoader(p *plugin.Plugin) (path string, err error) {
 	fname := fmt.Sprintf("plugin_%0d_%s.go", ctr, p.Name)
 	log.Printf("Generating loading code for plugin %s from %s into %s", p.Name, p.Repo, fname)
 
@@ -20,18 +21,28 @@ func generateLoader(p *plugin.Plugin) error {
 
 	f, err := os.Create(fname)
 	if err != nil {
-		return fmt.Errorf("cannot create file %s: ", fname, err)
+		return fname, fmt.Errorf("cannot create file %s: %s", fname, err)
 	}
 	defer f.Close()
 
 	loader, err := p.Loader()
 	if err != nil {
-		return fmt.Errorf("Could not create loader text: %s", err)
+		return fname, fmt.Errorf("Could not create loader text: %s", err)
 	}
 
 	fmt.Fprint(f, loader)
 
-	return nil
+	return fname, nil
+}
+
+func goFmt(file string) error {
+	c := exec.Command("go", "fmt", file)
+	out, err := c.CombinedOutput()
+	if err != nil {
+		log.Printf("go fmt failed: %s", string(out))
+	}
+
+	return err
 }
 
 func parseAndGenerate(file string) error {
@@ -41,10 +52,15 @@ func parseAndGenerate(file string) error {
 	}
 
 	for _, p := range list.Plugins {
-		err := generateLoader(p)
+		f, err := generateLoader(p)
 		if err != nil {
 			log.Printf("Could not generate loader for %s: %s", p.Name, err)
-			err = fmt.Errorf("some plugins failed to generate")
+			return fmt.Errorf("some plugins failed to generate")
+		}
+
+		err = goFmt(f)
+		if err != nil {
+			log.Printf("Could not go fmt %s: %s", f, err)
 		}
 	}
 
