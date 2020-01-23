76c87fa29d4c303389d07bcf0f22dfdaad27315e
Michael Porter <michael.porter@ingramcontent.com>
2020-01-23 16:37:45 -0500

    (#778) force architecture in wixl command

--- packager/templates/windows/global/build.sh
+++ packager/templates/windows/global/build.sh
@@ -5,5 +5,5 @@ set -x
 apt-get install wixl
 find .
 cd {{cpkg_name}}-{{cpkg_version}}
-wixl -o {{cpkg_name}}-{{cpkg_version}}.msi dist/choria.wxs
+wixl -o {{cpkg_name}}-{{cpkg_version}}.msi -a x64 dist/choria.wxs
 mv {{cpkg_name}}-{{cpkg_version}}.msi ..
