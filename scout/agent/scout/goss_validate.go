802825cf705c2b325aa387338863e4f239823293
R.I.Pienaar <rip@devco.net>
2020-07-17 00:04:03 +0200

    (#948) allow adhoc goss validates
    Signed-off-by: R.I.Pienaar <rip@devco.net>

---
+++ scout/agent/scout/goss_validate.go
@@ -0,0 +1,78 @@
+package agent
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/aelsabbahy/goss"
+	gossoutputs "github.com/aelsabbahy/goss/outputs"
+	gossutil "github.com/aelsabbahy/goss/util"
+
+	"github.com/choria-io/go-choria/choria"
+	"github.com/choria-io/go-choria/providers/agent/mcorpc"
+)
+
+type GossValidateRequest struct {
+	File string `json:"file"`
+	Vars string `json:"vars"`
+}
+
+type GossValidateResponse struct {
+	Failures int                                `json:"failures"`
+	Results  []gossoutputs.StructuredTestResult `json:"results"`
+	Runtime  float64                            `json:"runtime"`
+	Success  int                                `json:"success"`
+	Summary  string                             `json:"summary"`
+	Tests    int                                `json:"tests"`
+}
+
+func gossValidateAction(_ context.Context, req *mcorpc.Request, reply *mcorpc.Reply, agent *mcorpc.Agent, _ choria.ConnectorInfo) {
+	resp := &GossValidateResponse{Results: []gossoutputs.StructuredTestResult{}}
+	reply.Data = resp
+
+	args := &GossValidateRequest{}
+	if !mcorpc.ParseRequestData(args, req, reply) {
+		return
+	}
+
+	var out bytes.Buffer
+
+	opts := []gossutil.ConfigOption{
+		gossutil.WithMaxConcurrency(1),
+		gossutil.WithResultWriter(&out),
+		gossutil.WithSpecFile(args.File),
+	}
+
+	if args.Vars != "" {
+		opts = append(opts, gossutil.WithVarsFile(args.File))
+	}
+
+	cfg, err := gossutil.NewConfig(opts...)
+	if err != nil {
+		abort(fmt.Sprintf("Could not create Goss config: %s", err), reply)
+		return
+	}
+
+	_, err = goss.Validate(cfg, time.Now())
+	if err != nil {
+		abort(fmt.Sprintf("Could not validate: %s", err), reply)
+		return
+	}
+
+	res := &gossoutputs.StructuredOutput{}
+	err = json.Unmarshal(out.Bytes(), res)
+	if err != nil {
+		abort(fmt.Sprintf("Could not parse goss results: %s", err), reply)
+		return
+	}
+
+	resp.Results = res.Results
+	resp.Summary = res.SummaryLine
+	resp.Failures = res.Summary.Failed
+	resp.Runtime = res.Summary.TotalDuration.Seconds()
+	resp.Success = res.Summary.TestCount - res.Summary.Failed
+	resp.Tests = res.Summary.TestCount
+}
--- scout/agent/scout/scout.ddl
+++ scout/agent/scout/scout.ddl
@@ -78,6 +78,66 @@ action "maintenance", :description => "Pause checking of one or more checks" do
 
 end
 
+action "goss_validate", :description => "Performs a Goss validation using a specific file" do
+  display :always
+
+  input :file,
+        :prompt      => "Goss File",
+        :description => "Path to the Goss validation specification",
+        :type        => :string,
+        :validation  => '.+',
+        :maxlength   => 256,
+        :optional    => false
+
+
+  input :vars,
+        :prompt      => "Vars File",
+        :description => "Path to a file to use as template variables",
+        :type        => :string,
+        :validation  => '.+',
+        :maxlength   => 256,
+        :optional    => true
+
+
+
+
+  output :failures,
+         :description => "The number of tests that failed",
+         :type        => "integer",
+         :display_as  => "Failed Tests"
+
+  output :results,
+         :description => "The full test results",
+         :type        => "array",
+         :display_as  => "Results"
+
+  output :runtime,
+         :description => "The time it took to run the tests, in seconds",
+         :type        => "integer",
+         :display_as  => "Runtime"
+
+  output :success,
+         :description => "Indicates if the test passed",
+         :type        => "string",
+         :display_as  => "Success"
+
+  output :summary,
+         :description => "A human friendly test result",
+         :type        => "string",
+         :display_as  => "Summary"
+
+  output :tests,
+         :description => "The number of tests that were run",
+         :type        => "integer",
+         :display_as  => "Tests"
+
+  summarize do
+    aggregate summary(:tests)
+    aggregate summary(:failures)
+    aggregate summary(:success)
+  end
+end
+
 action "trigger", :description => "Force an immediate check of one or more checks" do
   display :failed
 
