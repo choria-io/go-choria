2ab8e637b328a01a7cb606ce201dca088823c991
R.I.Pienaar <rip@devco.net>
2020-12-10 10:45:11 +0100

    (misc) fire the correct event after timer ends
    Fixes a regression introduced while refactoring
    
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- aagent/watchers/schedulewatcher/state_notification.go
+++ aagent/watchers/schedulewatcher/state_notification.go
@@ -29,5 +29,5 @@ func (s *StateNotification) CloudEvent() cloudevents.Event {
 
 // String is a string representation of the notification suitable for printing
 func (s *StateNotification) String() string {
-	return fmt.Sprintf("%s %s#%s state: %s", s.Identity, s.Machine, s.Name, s.State)
+	return fmt.Sprintf("%s %s#%s schedule state: %s", s.Identity, s.Machine, s.Name, s.State)
 }
