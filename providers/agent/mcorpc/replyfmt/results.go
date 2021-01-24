choria-io/go-choria

commit cebe2cd78c63f1260d5198945b9c2cc67626a691

(#1086) create choria facts command
also fix issues with the DDL cache and various generated
client weirdnesses

Signed-off-by: R.I.Pienaar <rip@devco.net>

providers/agent/mcorpc/replyfmt/results.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"sort"
+	"strconv"
 	"strings"
 	"text/tabwriter"
 	"time"
@@ -88,7 +89,19 @@ func (r *RPCResults) RenderTXTFooter(w io.Writer, verbose bool) {
 		fmt.Fprintf(w, "          Agent Time: %v\n", stats.RequestTime-stats.PublishTime)
 		fmt.Fprintf(w, "          Total Time: %v\n", stats.RequestTime+stats.DiscoverTime)
 	} else {
-		fmt.Fprintf(w, "Finished processing %d / %d hosts in %s\n", stats.ResponseCount, stats.DiscoveredCount, stats.RequestTime+stats.DiscoverTime)
+		var rcnt, dcnt string
+
+		switch {
+		case stats.ResponseCount == 0:
+			rcnt = color.RedString(strconv.Itoa(stats.ResponseCount))
+		case stats.ResponseCount != stats.DiscoveredCount:
+			rcnt = color.YellowString(strconv.Itoa(stats.ResponseCount))
+		default:
+			dcnt = color.GreenString(strconv.Itoa(stats.DiscoveredCount))
+			rcnt = color.GreenString(strconv.Itoa(stats.ResponseCount))
+		}
+
+		fmt.Fprintf(w, "Finished processing %s / %s hosts in %s\n", rcnt, dcnt, (stats.RequestTime + stats.DiscoverTime).Round(time.Millisecond))
 	}
 
 	nodeListPrinter := func(nodes []string, message string) {

choria/util.go
@@ -23,7 +23,7 @@ func UserConfig() string {
 
 	if home != "" {
 		// TODO: .choria must go
-		for _, n := range []string{".choriarc", ".choria", ".mcollective"} {
+		for _, n := range []string{".choriarc", ".mcollective"} {
 			homeCfg := filepath.Join(home, n)
 
 			if FileExist(homeCfg) {
@@ -38,11 +38,6 @@ func UserConfig() string {
 
 	if FileExist("/etc/choria/client.conf") {
 		return "/etc/choria/client.conf"
-	}
-
-	// TODO: these must go
-	if FileExist("/etc/choria/client.cfg") {
-		return "/etc/choria/client.cfg"
 	}
 
 	if FileExist("/usr/local/etc/choria/client.conf") {
@@ -215,13 +210,18 @@ func BuildInfo() *build.Info {
 	return &build.Info{}
 }
 
-// ContainsString determines if needle is in hay
-func ContainsString(hay []string, needle string) bool {
-	for _, item := range hay {
-		if item == needle {
-			return true
+// LongestString determines the length of the longest string in list, capped at max
+func LongestString(list []string, max int) int {
+	longest := 0
+	for _, i := range list {
+		if len(i) > longest {
+			longest = len(i)
+		}
+
+		if longest > max {
+			return max
 		}
 	}
 
-	return false
+	return longest
 }

client/choria_utilclient/action_info.go
@@ -1,4 +1,4 @@
-// generated code; DO NOT EDIT; 2021-01-12 09:27:23.546124 +0100 CET m=+0.061442179"
+// generated code; DO NOT EDIT; 2021-01-12 18:43:24.312755 +0100 CET m=+0.056825610"
 //
 // Client for Choria RPC Agent 'choria_util'' Version 0.19.0 generated using Choria version 0.18.0
 
@@ -169,7 +169,12 @@ func (d *InfoResult) EachOutput(h func(r *InfoOutput)) {
 //
 // Description: Choria version
 func (d *InfoOutput) ChoriaVersion() interface{} {
-	val := d.reply["choria_version"]
+	val, ok := d.reply["choria_version"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -177,7 +182,12 @@ func (d *InfoOutput) ChoriaVersion() interface{} {
 //
 // Description: Middleware client gem flavour
 func (d *InfoOutput) ClientFlavour() interface{} {
-	val := d.reply["client_flavour"]
+	val, ok := d.reply["client_flavour"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -185,7 +195,12 @@ func (d *InfoOutput) ClientFlavour() interface{} {
 //
 // Description: Active Middleware client gem options
 func (d *InfoOutput) ClientOptions() interface{} {
-	val := d.reply["client_options"]
+	val, ok := d.reply["client_options"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -193,7 +208,12 @@ func (d *InfoOutput) ClientOptions() interface{} {
 //
 // Description: Middleware client gem statistics
 func (d *InfoOutput) ClientStats() interface{} {
-	val := d.reply["client_stats"]
+	val, ok := d.reply["client_stats"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -201,7 +221,12 @@ func (d *InfoOutput) ClientStats() interface{} {
 //
 // Description: Middleware client library version
 func (d *InfoOutput) ClientVersion() interface{} {
-	val := d.reply["client_version"]
+	val, ok := d.reply["client_version"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -209,7 +234,12 @@ func (d *InfoOutput) ClientVersion() interface{} {
 //
 // Description: Connected middleware server
 func (d *InfoOutput) ConnectedServer() interface{} {
-	val := d.reply["connected_server"]
+	val, ok := d.reply["connected_server"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -217,7 +247,12 @@ func (d *InfoOutput) ConnectedServer() interface{} {
 //
 // Description: Connector plugin
 func (d *InfoOutput) Connector() interface{} {
-	val := d.reply["connector"]
+	val, ok := d.reply["connector"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -225,7 +260,12 @@ func (d *InfoOutput) Connector() interface{} {
 //
 // Description: If the connector is running with TLS security enabled
 func (d *InfoOutput) ConnectorTls() interface{} {
-	val := d.reply["connector_tls"]
+	val, ok := d.reply["connector_tls"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -233,7 +273,12 @@ func (d *InfoOutput) ConnectorTls() interface{} {
 //
 // Description: Command used for Facter
 func (d *InfoOutput) FacterCommand() interface{} {
-	val := d.reply["facter_command"]
+	val, ok := d.reply["facter_command"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -241,7 +286,12 @@ func (d *InfoOutput) FacterCommand() interface{} {
 //
 // Description: Facter domain
 func (d *InfoOutput) FacterDomain() interface{} {
-	val := d.reply["facter_domain"]
+	val, ok := d.reply["facter_domain"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -249,7 +299,12 @@ func (d *InfoOutput) FacterDomain() interface{} {
 //
 // Description: Middleware Servers configured or discovered
 func (d *InfoOutput) MiddlewareServers() interface{} {
-	val := d.reply["middleware_servers"]
+	val, ok := d.reply["middleware_servers"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -257,7 +312,12 @@ func (d *InfoOutput) MiddlewareServers() interface{} {
 //
 // Description: Active OS PATH
 func (d *InfoOutput) Path() interface{} {
-	val := d.reply["path"]
+	val, ok := d.reply["path"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -265,7 +325,12 @@ func (d *InfoOutput) Path() interface{} {
 //
 // Description: If the protocol is running with PKI security enabled
 func (d *InfoOutput) SecureProtocol() interface{} {
-	val := d.reply["secure_protocol"]
+	val, ok := d.reply["secure_protocol"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -273,7 +338,12 @@ func (d *InfoOutput) SecureProtocol() interface{} {
 //
 // Description: Security Provider plugin
 func (d *InfoOutput) Security() interface{} {
-	val := d.reply["security"]
+	val, ok := d.reply["security"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -281,7 +351,12 @@ func (d *InfoOutput) Security() interface{} {
 //
 // Description: Configured SRV domain
 func (d *InfoOutput) SrvDomain() interface{} {
-	val := d.reply["srv_domain"]
+	val, ok := d.reply["srv_domain"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -289,6 +364,11 @@ func (d *InfoOutput) SrvDomain() interface{} {
 //
 // Description: Indicates if SRV records are considered
 func (d *InfoOutput) UsingSrv() interface{} {
-	val := d.reply["using_srv"]
+	val, ok := d.reply["using_srv"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }

client/choria_utilclient/action_machine_states.go
@@ -1,4 +1,4 @@
-// generated code; DO NOT EDIT; 2021-01-12 09:27:23.552849 +0100 CET m=+0.068166863"
+// generated code; DO NOT EDIT; 2021-01-12 18:43:24.317825 +0100 CET m=+0.061895566"
 //
 // Client for Choria RPC Agent 'choria_util'' Version 0.19.0 generated using Choria version 0.18.0
 
@@ -169,7 +169,12 @@ func (d *MachineStatesResult) EachOutput(h func(r *MachineStatesOutput)) {
 //
 // Description: List of running machine IDs
 func (d *MachineStatesOutput) MachineIds() interface{} {
-	val := d.reply["machine_ids"]
+	val, ok := d.reply["machine_ids"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -177,7 +182,12 @@ func (d *MachineStatesOutput) MachineIds() interface{} {
 //
 // Description: List of running machine names
 func (d *MachineStatesOutput) MachineNames() interface{} {
-	val := d.reply["machine_names"]
+	val, ok := d.reply["machine_names"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }
 
@@ -185,6 +195,11 @@ func (d *MachineStatesOutput) MachineNames() interface{} {
 //
 // Description: Hash map of machine statusses indexed by machine ID
 func (d *MachineStatesOutput) States() interface{} {
-	val := d.reply["states"]
+	val, ok := d.reply["states"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }

client/choria_utilclient/action_machine_transition.go
@@ -1,4 +1,4 @@
-// generated code; DO NOT EDIT; 2021-01-12 09:27:23.559381 +0100 CET m=+0.074698931"
+// generated code; DO NOT EDIT; 2021-01-12 18:43:24.320557 +0100 CET m=+0.064627823"
 //
 // Client for Choria RPC Agent 'choria_util'' Version 0.19.0 generated using Choria version 0.18.0
 
@@ -205,6 +205,11 @@ func (d *MachineTransitionRequester) Version(v string) *MachineTransitionRequest
 //
 // Description: Indicates if the transition was successfully accepted
 func (d *MachineTransitionOutput) Success() interface{} {
-	val := d.reply["success"]
+	val, ok := d.reply["success"]
+	if !ok || val == nil {
+		// we have to avoid returning nil.(interface{})
+		return nil
+	}
+
 	return val.(interface{})
 }

client/choria_utilclient/client.go
@@ -165,6 +165,10 @@ func New(opts ...InitializationOption) (client *ChoriaUtilClient, err error) {
 
 	c.cfg = c.fw.Configuration()
 
+	if c.clientOpts.dt > 0 {
+		c.cfg.DiscoveryTimeout = int(c.clientOpts.dt.Seconds())
+	}
+
 	if c.clientOpts.ns == nil {
 		switch c.cfg.DefaultDiscoveryMethod {
 		case "choria":

client/choria_utilclient/doc.go
@@ -1,4 +1,4 @@
-// generated code; DO NOT EDIT; 2021-01-12 09:27:23.586091 +0100 CET m=+0.101409488"
+// generated code; DO NOT EDIT; 2021-01-12 18:43:24.337357 +0100 CET m=+0.081428465"
 //
 // Client for Choria RPC Agent 'choria_util'' Version 0.19.0 generated using Choria version 0.18.0
 

client/choria_utilclient/initoptions.go
@@ -3,6 +3,8 @@
 package choria_utilclient
 
 import (
+	"time"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -10,6 +12,7 @@ type initOptions struct {
 	cfgFile  string
 	logger   *logrus.Entry
 	ns       NodeSource
+	dt       time.Duration
 	progress bool
 }
 
@@ -41,5 +44,25 @@ func Discovery(ns NodeSource) InitializationOption {
 func Progress() InitializationOption {
 	return func(o *initOptions) {
 		o.progress = true
+	}
+}
+
+// DiscoveryMethod accepts a discovery method name as supplied from the CLI and configures the correct NodeSource
+// reverts to broadcast method if an unsupported method is supplied, custom node sources can be set using Discovery()
+func DiscoveryMethod(m string) InitializationOption {
+	return func(o *initOptions) {
+		switch m {
+		case "choria", "puppetdb", "pdb":
+			o.ns = &PuppetDBNS{}
+		default:
+			o.ns = &BroadcastNS{}
+		}
+	}
+}
+
+// DiscoveryTimeout sets a timeout for discovery for those methods that support it
+func DiscoveryTimeout(t time.Duration) InitializationOption {
+	return func(o *initOptions) {
+		o.dt = t
 	}
 }

commit 0229e0d9cfebdce06bcb3b7d5da6c949ff268d88

(misc) Fix display of discovered nodes count in reply footer
A regression was introduced in cebe2cd78c63f1260d5198945b9c2cc67626a691
where the number of discovered nodes is not displayed if this number is
0 or if the number of responses is different from the number of
discovered nodes.

Properly initialize the string representation of the number of
discovered nodes to fix this issue.

providers/agent/mcorpc/replyfmt/results.go
@@ -93,8 +93,10 @@ func (r *RPCResults) RenderTXTFooter(w io.Writer, verbose bool) {
 
 		switch {
 		case stats.ResponseCount == 0:
+			dcnt = color.RedString(strconv.Itoa(stats.DiscoveredCount))
 			rcnt = color.RedString(strconv.Itoa(stats.ResponseCount))
 		case stats.ResponseCount != stats.DiscoveredCount:
+			dcnt = color.YellowString(strconv.Itoa(stats.DiscoveredCount))
 			rcnt = color.YellowString(strconv.Itoa(stats.ResponseCount))
 		default:
 			dcnt = color.GreenString(strconv.Itoa(stats.DiscoveredCount))