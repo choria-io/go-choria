c9542c2445b8ffafa535f57173275a6ec1e560ef
R.I.Pienaar <rip@devco.net>
2021-09-19 10:50:46 +0200

    (#1428) initial cli api helper for governors
    Signed-off-by: R.I.Pienaar <rip@devco.net>

---
+++ cmd/governor_api.go
@@ -0,0 +1,224 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/choria-io/go-choria/config"
+	"github.com/nats-io/jsm.go"
+	"github.com/nats-io/jsm.go/governor"
+)
+
+type tGovAPICommand struct {
+	command
+	update bool
+	list   bool
+	delete bool
+	check  bool
+
+	name     string
+	limit    int64
+	expire   int
+	replicas int
+	force    bool
+}
+
+func (g *tGovAPICommand) Setup() (err error) {
+	if gov, ok := cmdWithFullCommand("governor"); ok {
+		g.cmd = gov.Cmd().Command("api", "API to manage Governors via JSON inputs and outputs").Hidden()
+		g.cmd.Flag("ensure", "Creates or Updates the governor based on supplied configuration").BoolVar(&g.update)
+		g.cmd.Flag("delete", "Deletes a specific governor").PlaceHolder("GOVERNOR").BoolVar(&g.delete)
+		g.cmd.Flag("list", "List known governors").BoolVar(&g.list)
+		g.cmd.Flag("check", "Checks if the API is available").BoolVar(&g.check)
+
+		g.cmd.Flag("name", "Governor name").PlaceHolder("NAME").StringVar(&g.name)
+		g.cmd.Flag("capacity", "Governor capacity").PlaceHolder("CAPACITY").Int64Var(&g.limit)
+		g.cmd.Flag("expire", "How long before entries expire from the governor").PlaceHolder("SECONDS").IntVar(&g.expire)
+		g.cmd.Flag("replicas", "How many replicas to store on the server").PlaceHolder("REPLICAS").IntVar(&g.replicas)
+		g.cmd.Flag("force", "Force changes that require the governor to be recreated").BoolVar(&g.force)
+	}
+
+	return nil
+}
+
+func (g *tGovAPICommand) Configure() error {
+	if os.Getuid() == 0 {
+		cfg, err = config.NewSystemConfig(configFile, true)
+		if err != nil {
+			return err
+		}
+		cfg.LogLevel = "error"
+	} else {
+		return commonConfigure()
+	}
+
+	return nil
+}
+
+func (g *tGovAPICommand) Run(wg *sync.WaitGroup) (err error) {
+	defer wg.Done()
+
+	switch {
+	case g.check:
+		g.jsonDump(map[string]string{"api": "ok"})
+	case g.update:
+		g.updateCmd()
+	case g.delete:
+		g.deleteCmd()
+	case g.list:
+		g.listCmd()
+	default:
+		g.fail("no command given")
+	}
+
+	return nil
+}
+
+func (g *tGovAPICommand) updateCmd() {
+	switch {
+	case g.name == "":
+		g.fail("name required")
+	case g.limit == 0:
+		g.fail("capacity can not be 0")
+	case g.expire == 0:
+		g.fail("expire can not be 0")
+	case g.replicas < 1 || g.replicas > 5:
+		g.fail("replicas should be 1-5")
+	}
+
+	conn, err := c.NewConnector(ctx, c.MiddlewareServers, fmt.Sprintf("governor manager: %s", "governor_list"), c.Logger("governor"))
+	if err != nil {
+		g.fail("connection failed: %s", err)
+	}
+
+	mgr, err := jsm.New(conn.Nats())
+	if err != nil {
+		g.fail("connection failed: %s", err)
+	}
+
+	gov, err := governor.NewJSGovernorManager(g.name, uint64(g.limit), time.Duration(g.expire)*time.Second, uint(g.replicas), mgr, true, governor.WithSubject(c.GovernorSubject(g.name)))
+	if err != nil {
+		g.fail("update failed: %s", err)
+	}
+
+	if gov.Replicas() != g.replicas {
+		if !g.force {
+			g.fail("replica update required force")
+		}
+
+		err = gov.Stream().Delete()
+		if err != nil {
+			g.fail("deleting existing stream failed: %s", err)
+		}
+
+		gov, err = governor.NewJSGovernorManager(g.name, uint64(g.limit), time.Duration(g.expire)*time.Second, uint(g.replicas), mgr, true)
+		if err != nil {
+			g.fail("update failed: %s", err)
+		}
+	}
+
+	g.jsonDump(map[string]interface{}{
+		"name":     gov.Name(),
+		"capacity": gov.Limit(),
+		"expire":   gov.MaxAge().Seconds(),
+		"replicas": gov.Replicas(),
+	})
+}
+
+func (g *tGovAPICommand) deleteCmd() {
+	if g.name == "" {
+		g.fail("no name given")
+	}
+
+	conn, err := c.NewConnector(ctx, c.MiddlewareServers, fmt.Sprintf("governor manager: %s", "governor_list"), c.Logger("governor"))
+	if err != nil {
+		g.fail("connection failed: %s", err)
+	}
+
+	mgr, err := jsm.New(conn.Nats())
+	if err != nil {
+		g.fail("connection failed: %s", err)
+	}
+
+	str, err := mgr.LoadStream(fmt.Sprintf("GOVERNOR_%s", g.name))
+	if err != nil {
+		if jsm.IsNatsError(err, 10059) {
+			return
+		}
+		g.fail("could not find governor: %s", err)
+	}
+
+	err = str.Delete()
+	if err != nil {
+		g.fail("delete failed: %s", err)
+	}
+}
+
+func (g *tGovAPICommand) listCmd() {
+	type gov struct {
+		Name     string `json:"name"`
+		Capacity int64  `json:"capacity"`
+		Expire   int    `json:"expire"`
+		Replicas int    `json:"replicas"`
+	}
+
+	conn, err := c.NewConnector(ctx, c.MiddlewareServers, fmt.Sprintf("governor manager: %s", "governor_list"), c.Logger("governor"))
+	if err != nil {
+		g.fail("connection failed: %s", err)
+	}
+
+	mgr, err := jsm.New(conn.Nats())
+	if err != nil {
+		g.fail("connection failed: %s", err)
+	}
+
+	known, err := mgr.StreamNames(&jsm.StreamNamesFilter{
+		Subject: c.GovernorSubject("*"),
+	})
+	if err != nil {
+		g.fail("connection failed: %s", err)
+	}
+
+	var govs = make([]gov, len(known))
+	for i := 0; i < len(known); i++ {
+		name := strings.TrimPrefix(known[i], "GOVERNOR_")
+
+		mgr, err := governor.NewJSGovernorManager(name, 0, 0, 1, mgr, false)
+		if err != nil {
+			g.fail("loading failed: %s", err)
+		}
+
+		govs[i] = gov{
+			Name:     name,
+			Capacity: mgr.Limit(),
+			Expire:   int(mgr.MaxAge().Seconds()),
+			Replicas: mgr.Replicas(),
+		}
+	}
+
+	g.jsonDump(govs)
+}
+
+func (g *tGovAPICommand) fail(format string, a ...interface{}) {
+	g.jsonDump(map[string]string{
+		"error": fmt.Sprintf(format, a...),
+	})
+
+	os.Exit(1)
+}
+
+func (g *tGovAPICommand) jsonDump(d interface{}) {
+	j, err := json.Marshal(d)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(j))
+}
+func init() {
+	cli.commands = append(cli.commands, &tGovAPICommand{})
+}
