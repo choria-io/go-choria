2fca0b97d16180e8f4a6118632cc48e7e97fc908
R.I.Pienaar <rip@devco.net>
2024-02-08 09:58:08 +0100

    (#2110) Adds a provisioner lock held for the first caller
    This will allow for active-active provisioners and ensure only
    one will be responsible for the node without state sharing in
    the provisioner side or elections
    
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- providers/agent/mcorpc/golang/provision/provision_test.go
+++ providers/agent/mcorpc/golang/provision/provision_test.go
@@ -109,6 +109,36 @@ var _ = Describe("Provision/Agent", func() {
 		})
 	})
 
+	Describe("lockedAction", func() {
+		It("Should obtain a lock and not let others interact while locked", func() {
+			build.ProvisionToken = ""
+			LockWindow = 500 * time.Millisecond
+
+			// first call will lock it for 2 minutes, we reach the wrapped action that produce a error
+			lockedAction(shutdownAction)(ctx, &mcorpc.Request{CallerID: "one", SenderID: "ginkgo"}, reply, prov, nil)
+			Expect(reply.Statuscode).To(Equal(mcorpc.Aborted))
+			Expect(reply.Statusmsg).To(Equal("Cannot shutdown a server that is not in provisioning mode or with no token set"))
+			Expect(lockedUntil).To(BeTemporally("~", time.Now().Add(LockWindow), time.Millisecond))
+			Expect(lockedBy).To(Equal("ginkgo.one"))
+
+			// now someone else calls and we expect locked error
+			lockedAction(shutdownAction)(ctx, &mcorpc.Request{CallerID: "two", SenderID: "ginkgo"}, reply, prov, nil)
+			Expect(reply.Statuscode).To(Equal(mcorpc.Aborted))
+			Expect(reply.Statusmsg).To(Equal(LockedError))
+
+			// but when we call we expect it to work and get through to the wrapped action
+			lockedAction(shutdownAction)(ctx, &mcorpc.Request{CallerID: "one", SenderID: "ginkgo"}, reply, prov, nil)
+			Expect(reply.Statuscode).To(Equal(mcorpc.Aborted))
+			Expect(reply.Statusmsg).To(Equal("Cannot shutdown a server that is not in provisioning mode or with no token set"))
+
+			// now we let the lock window pass, we expect another caller to be able to reach the server
+			time.Sleep(500 * time.Millisecond)
+			lockedAction(shutdownAction)(ctx, &mcorpc.Request{CallerID: "two", SenderID: "ginkgo"}, reply, prov, nil)
+			Expect(reply.Statuscode).To(Equal(mcorpc.Aborted))
+			Expect(reply.Statusmsg).To(Equal("Cannot shutdown a server that is not in provisioning mode or with no token set"))
+		})
+	})
+
 	Describe("ed25519Action", func() {
 		var td string
 
