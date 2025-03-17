ff4dca5046fe3b359d0c8c2da521c16c5d08a6f6
R.I.Pienaar <rip@devco.net>
2025-03-17 12:55:26 +0100

    (#2236) Allow machines run to start multiple machines
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- cmd/machine_run.go
+++ cmd/machine_run.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2021, R.I. Pienaar and the Choria Project contributors
+// Copyright (c) 2019-2025, R.I. Pienaar and the Choria Project contributors
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -18,16 +18,16 @@ import (
 
 type mRunCommand struct {
 	command
-	sourceDir string
-	factsFile string
-	dataFile  string
-	connect   bool
+	sourceDirs []string
+	factsFile  string
+	dataFile   string
+	connect    bool
 }
 
 func (r *mRunCommand) Setup() (err error) {
 	if machine, ok := cmdWithFullCommand("machine"); ok {
 		r.cmd = machine.Cmd().Command("run", "Runs an autonomous agent locally")
-		r.cmd.Arg("source", "Directory containing the machine definition").Required().ExistingDirVar(&r.sourceDir)
+		r.cmd.Arg("source", "Directories containing the machine definitions").Required().ExistingDirsVar(&r.sourceDirs)
 		r.cmd.Flag("facts", "JSON format facts file to supply to the machine as run time facts").ExistingFileVar(&r.factsFile)
 		r.cmd.Flag("data", "JSON format data file to supply to the machine as run time data").ExistingFileVar(&r.dataFile)
 		r.cmd.Flag("connect", "Connects to the Choria Broker when running the autonomous agent").UnNegatableBoolVar(&r.connect)
@@ -65,54 +65,56 @@ func (r *mRunCommand) Run(wg *sync.WaitGroup) (err error) {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
 
-	m, err := machine.FromDir(r.sourceDir, watchers.New(ctx))
-	if err != nil {
-		return err
-	}
-
-	m.SetIdentity("cli")
-	m.RegisterNotifier(&console.Notifier{})
-	m.SetMainCollective(cfg.MainCollective)
-
-	if r.factsFile != "" {
-		facts, err := os.ReadFile(r.factsFile)
+	for _, sourceDir := range r.sourceDirs {
+		m, err := machine.FromDir(sourceDir, watchers.New(ctx))
 		if err != nil {
 			return err
 		}
 
-		m.SetFactSource(func() json.RawMessage { return facts })
-	}
+		m.SetIdentity("cli")
+		m.RegisterNotifier(&console.Notifier{})
+		m.SetMainCollective(cfg.MainCollective)
 
-	if r.dataFile != "" {
-		dat := make(map[string]any)
-		df, err := os.ReadFile(r.dataFile)
-		if err != nil {
-			return err
-		}
-
-		err = json.Unmarshal(df, &dat)
-		if err != nil {
-			return err
-		}
-
-		for k, v := range dat {
-			err = m.DataPut(k, v)
+		if r.factsFile != "" {
+			facts, err := os.ReadFile(r.factsFile)
 			if err != nil {
 				return err
 			}
-		}
-	}
 
-	if r.connect {
-		conn, err := c.NewConnector(ctx, c.MiddlewareServers, "machine run", c.Logger("machine"))
-		if err != nil {
-			return err
+			m.SetFactSource(func() json.RawMessage { return facts })
 		}
 
-		m.SetConnection(conn)
-	}
+		if r.dataFile != "" {
+			dat := make(map[string]any)
+			df, err := os.ReadFile(r.dataFile)
+			if err != nil {
+				return err
+			}
 
-	<-m.Start(ctx, wg)
+			err = json.Unmarshal(df, &dat)
+			if err != nil {
+				return err
+			}
+
+			for k, v := range dat {
+				err = m.DataPut(k, v)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		if r.connect {
+			conn, err := c.NewConnector(ctx, c.MiddlewareServers, "machine run", c.Logger("machine"))
+			if err != nil {
+				return err
+			}
+
+			m.SetConnection(conn)
+		}
+
+		<-m.Start(ctx, wg)
+	}
 
 	<-ctx.Done()
 
